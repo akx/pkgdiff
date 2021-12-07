d03588533135c36eed6bdc65b6882c2bd2d879ba
Aarni Koskela <akx@iki.fi>
2021-12-07 16:06:51 +0200

    Add file diff via `git diff`

--- pkgdiff.py
+++ pkgdiff.py
@@ -1,10 +1,14 @@
 import argparse
+import shutil
+import subprocess
+import tempfile
+
 import dataclasses
 import datetime
 import os
 import tarfile
 import zipfile
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 
 @dataclasses.dataclass(frozen=True)
@@ -13,6 +17,7 @@ class ManifestEntry:
     size: int
     mtime: datetime.datetime
     original: Any
+    content: Optional[bytes] = None
 
 
 def compare_subset(a, b, keys) -> Dict[str, Any]:
@@ -25,7 +30,7 @@ def compare_subset(a, b, keys) -> Dict[str, Any]:
     return ret
 
 
-def read_tar_manifest(filename) -> Dict[str, ManifestEntry]:
+def read_tar_manifest(filename, *, keep_content: bool) -> Dict[str, ManifestEntry]:
     with tarfile.open(filename, "r") as tf:
         return {
             ti.name: ManifestEntry(
@@ -33,13 +38,16 @@ def read_tar_manifest(filename) -> Dict[str, ManifestEntry]:
                 size=ti.size,
                 mtime=datetime.datetime.fromtimestamp(ti.mtime),
                 original=ti,
+                content=(
+                    tf.extractfile(ti).read() if ti.isreg() and keep_content else None
+                ),
             )
             for ti in tf.getmembers()
             if not ti.isdir()
         }
 
 
-def read_zip_manifest(filename) -> Dict[str, ManifestEntry]:
+def read_zip_manifest(filename, *, keep_content: bool) -> Dict[str, ManifestEntry]:
     with zipfile.ZipFile(filename, "r") as zf:
         return {
             info.filename: ManifestEntry(
@@ -47,17 +55,18 @@ def read_zip_manifest(filename) -> Dict[str, ManifestEntry]:
                 size=info.file_size,
                 mtime=datetime.datetime(*info.date_time),
                 original=info,
+                content=(zf.read(info) if keep_content else None),
             )
             for info in (zf.getinfo(name) for name in zf.namelist())
         }
 
 
-def read_package_manifest(filename: str):
+def read_package_manifest(filename: str, *, keep_content: bool = False):
     ext = os.path.splitext(filename)[-1].lower()
     if ".tar." in filename or ext == ".tgz":
-        return read_tar_manifest(filename)
+        return read_tar_manifest(filename, keep_content=keep_content)
     if ext in (".zip", ".whl"):
-        return read_zip_manifest(filename)
+        return read_zip_manifest(filename, keep_content=keep_content)
     raise ValueError(f"No reader for {filename}")
 
 
@@ -83,12 +92,16 @@ def main():
     ap.add_argument("files", metavar="FILE", nargs="+")
     ap.add_argument("--strip", type=int, default=0)
     ap.add_argument("--compare-mtime", action="store_true", default=False)
+    ap.add_argument("--show-diff", action="store_true", default=False)
     args = ap.parse_args()
     if len(args.files) != 2:
         raise ValueError("Only 2 files are supported at this time.")
     f1, f2 = args.files
-    m1 = strip_names(read_package_manifest(f1), strip=args.strip)
-    m2 = strip_names(read_package_manifest(f2), strip=args.strip)
+    show_diff = args.show_diff
+    m1 = read_package_manifest(f1, keep_content=show_diff)
+    m1 = strip_names(m1, strip=args.strip)
+    m2 = read_package_manifest(f2, keep_content=show_diff)
+    m2 = strip_names(m2, strip=args.strip)
 
     only_in_1, only_in_2, common = diff_common(m1, m2)
     if only_in_1:
@@ -110,6 +123,22 @@ def main():
             diffs = compare_subset(e1, e2, keys=comparison_keys)
             if diffs:
                 print(filename, diffs)
+                if show_diff and (e1.content and e2.content):
+                    show_file_diff(filename, e1, e2)
+
+
+def show_file_diff(filename: str, e1: ManifestEntry, e2: ManifestEntry) -> None:
+    ext = os.path.splitext(filename)[-1]
+    tfa = tempfile.NamedTemporaryFile(prefix="a", suffix=ext, delete=True)
+    tfb = tempfile.NamedTemporaryFile(prefix="b", suffix=ext, delete=True)
+    with tfa, tfb:
+        tfa.write(e1.content)
+        tfa.flush()
+        tfb.write(e2.content)
+        tfb.flush()
+        subprocess.call(
+            [shutil.which("git"), "diff", "--no-index", "--", tfa.name, tfb.name]
+        )
 
 
 if __name__ == "__main__":
